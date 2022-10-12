fca374e5991d1692cb90691a1ffb181d59e6b69c
jcledgett <jcledgett@gmail.com>
2022-10-12 15:53:11 -0700

    update

---
+++ NAT.ps1
@@ -0,0 +1,9 @@
+#create new virtual switch
+#Import-Module PSReadLine
+New-VMSwitch -SwitchName LabSwitch -SwitchType Internal
+ 
+Get-NetAdapter
+#get from new switch created ifIndex
+#New-NetIPAddress -IPAddress 192.168.1.1 -Prefixlength 24 -InterfaceIndex 70
+#New-NetNat -Name LabNAT -InternalIPInterfaceAddressPrefix 192.168.1.0/24
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DataLake_Setup
  Project using Min.io, Apache HUDI, StarRocks.io and BI tools
+# Setting up a Business Intelligence Data Lake environment.
+
+
